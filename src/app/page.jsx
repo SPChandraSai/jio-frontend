a26046edec50753bf75d19caf86e6bf16612a93f
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-04-29 16:14:04 +0530

    frontend: ui for home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,30 @@
-import Image from "next/image";
+import BannerSection from "@/components/section/BannerSection";
+import CategoriesSection from "@/components/section/CategoriesSection";
+import Jumper from "@/components/section/Jumper";
 
 export default function Home() {
+  const list=[
+    {
+      label:"Top Rated",
+      href:"top-rated",
+    },
+    {
+      label:"Popular",
+      href:"popular",
+    },
+    {
+      label:"Upcoming",
+      href:"upcoming",
+    },
+  ];
   return (
-    <h1 className="h-[60vh] text-black text-3xl flex justify-center items-center">Home Page</h1>
-  )
+    <>
+      <Jumper list={list}/>
+      <BannerSection></BannerSection>
+      {/* // list of categories*/}
+      {list.map((item)=>{
+        return <CategoriesSection key={item.label} title={item.label} id={item.href} />
+      })}
+    </>
+  );
 }
