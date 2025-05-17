8459faebabd87b22a12d9fc3626aae5cdf818dd2
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-05-17 15:28:07 +0530

    jio class-12 login signup api integration

--- src/lib/api.js
+++ src/lib/api.js
@@ -54,4 +54,6 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://loca
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
+    // credentials
+    withCredentials: true,
 });
