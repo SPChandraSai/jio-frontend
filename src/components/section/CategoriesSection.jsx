fe829221fde411c781fecdfed6fe0aad6c9306cd
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-06-03 17:07:25 +0530

    Jio clone frontend 18: wishlist page added

--- src/components/section/CategoriesSection.jsx
+++ src/components/section/CategoriesSection.jsx
@@ -51,7 +51,7 @@ async function CategoriesContent({ fetcher }) {
 
 }
 
-function CategoriesFallback() {
+export function CategoriesFallback() {
   return (
     <ul className="flex gap-4 w-full overflow-scroll scrollbar-hide">
       {new Array(12).fill(0).map((e, index) => (
